5f126ac3ee930828ee18e47b0afe47eed23c72e4
macbookpro <volodyathehurricane@gmail.com>
2018-07-26 22:41:04 +0300

    конфиг для хостинга

---
+++ config-web.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://komatsu-rus.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://komatsu-rus.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ih48836/public_html2/komatsu-rus.ru/catalog/');
+define('DIR_SYSTEM', '/home/ih48836/public_html2/komatsu-rus.ru/system/');
+define('DIR_DATABASE', '/home/ih48836/public_html2/komatsu-rus.ru/system/database/');
+define('DIR_LANGUAGE', '/home/ih48836/public_html2/komatsu-rus.ru/catalog/language/');
+define('DIR_TEMPLATE', '/home/ih48836/public_html2/komatsu-rus.ru/catalog/view/theme/');
+define('DIR_CONFIG', '/home/ih48836/public_html2/komatsu-rus.ru/system/config/');
+define('DIR_IMAGE', '/home/ih48836/public_html2/komatsu-rus.ru/image/');
+define('DIR_CACHE', '/home/ih48836/public_html2/komatsu-rus.ru/system/cache/');
+define('DIR_DOWNLOAD', '/home/ih48836/public_html2/komatsu-rus.ru/download/');
+define('DIR_LOGS', '/home/ih48836/public_html2/komatsu-rus.ru/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ih48836_artyom');
+define('DB_PASSWORD', 'Artyom25');
+define('DB_DATABASE', 'ih48836_komatsu-rus');
+define('DB_PREFIX', 'oc_');
+?>
